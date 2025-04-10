1bb10f7050625da94495fe77a9e5e6b9f0292d23
RajeshBodatech <rajeshboda30@gmail.com>
2025-04-10 13:51:51 +0530

    We are commiting the java file

---
+++ Example.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Example
+{
+public static void main(String args[])
+{
+System.out.println("Hello all This is the Example Java File");
+System.out.println("we demonstrating the CI/CD process of jenkins");
+}
+}
