aa6d673bc62b396918bfc9c281fc34ffc4de0f69
Boda Rajesh <154888255+RajeshBodatech@users.noreply.github.com>
2025-04-22 09:38:10 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -6,5 +6,6 @@ public static void main(String args[])
 System.out.println("Hello all This is the Example Java File");
 System.out.println("we demonstrating the CI/CD process of jenkins");
 System.out.println("WELCOME TO PROGRAMMING world WORLD");
+System.out.println("Demonstrating the Jenkins using github for external exams");
 }
 }
